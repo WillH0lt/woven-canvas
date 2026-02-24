56fd9bbf95287b0a9516b230c4a8fde800c4aea0
will whiteneck <will.whiteneck@gmail.com>
2026-02-24 15:05:56 -0700

    add sitemap

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://woven-canvas.dev",
   vite: {
     resolve: {
       conditions: ['@woven-canvas/source'],
